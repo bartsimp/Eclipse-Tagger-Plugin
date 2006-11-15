07809b6504aab29ee9fdf64e70e673cd27650796
cjstehno <cjstehno@e4b1bbd4-2222-0410-bd84-e81ad0756338>
2006-11-15 15:00:31 +0000

    Added Tag and ITaggable starting points.

--- net.sourceforge.taggerplugin/src/main/java/net/sourceforge/taggerplugin/TaggerActivator.java
+++ net.sourceforge.taggerplugin/src/main/java/net/sourceforge/taggerplugin/TaggerActivator.java
@@ -4,33 +4,29 @@ import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
 /**
- * The activator class controls the plug-in life cycle
+ * The activator class controls the plug-in life cycle.
+ *
+ * @author Christopher J. Stehno (chris@stehno.com)
  */
 public class TaggerActivator extends AbstractUIPlugin {
 
-	// The plug-in ID
-	public static final String PLUGIN_ID = "com.stehno.eclipse.tagger";
-
-	// The shared instance
+	public static final String PLUGIN_ID = "net.sourceforge.taggerplugin";
 	private static TaggerActivator plugin;
-	
-	/**
-	 * The constructor
-	 */
+
 	public TaggerActivator() {
 		plugin = this;
 	}
 
-	/*
-	 * (non-Javadoc)
+	/**
+	 *
 	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
 	 */
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 	}
 
-	/*
-	 * (non-Javadoc)
+	/**
+	 *
 	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
 	 */
 	public void stop(BundleContext context) throws Exception {
@@ -46,5 +42,4 @@ public class TaggerActivator extends AbstractUIPlugin {
 	public static TaggerActivator getDefault() {
 		return plugin;
 	}
-
 }
